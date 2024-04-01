c050ecdc6bd0b7ead3435da952644e75d9424c1b
Keila De la torre solis <keila.solis.123@outlook.com>
2024-04-01 15:42:41 -0600

    Agregar archivos de proyecto.

---
+++ AppXML/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace AppXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRead = new System.Windows.Forms.Button();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(36, 55);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 0;
+            this.btnRead.Text = "READ";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click_1);
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(36, 104);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowHeadersWidth = 51;
+            this.dgvData.RowTemplate.Height = 24;
+            this.dgvData.Size = new System.Drawing.Size(705, 309);
+            this.dgvData.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.DataGridView dgvData;
+    }
+}
+
---
+++ AppXML/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace AppXML
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static void WriteXmlDocument()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNode root = doc.CreateElement("Employees");
+            doc.AppendChild(root);
+
+            XmlNode employeeNode = doc.CreateElement("Employee");
+            XmlAttribute attribute = doc.CreateAttribute("Name");           
+            attribute.Value = "Keila";
+            employeeNode.Attributes.Append(attribute);
+            XmlNode lastNameNode = doc.CreateElement("LastName");
+            lastNameNode.InnerText = "de la Torre";
+            XmlNode ageNode = doc.CreateElement("Age");
+            ageNode.InnerText = "22";
+            employeeNode.AppendChild(lastNameNode);
+            employeeNode.AppendChild(ageNode);
+            root.AppendChild(employeeNode);
+
+            employeeNode = doc.CreateElement("Employee");
+            attribute = doc.CreateAttribute("Name");
+            attribute.Value = "Luis";
+            employeeNode.Attributes.Append(attribute);
+            lastNameNode = doc.CreateElement("LastName");
+            lastNameNode.InnerText = "Fabela";
+            ageNode = doc.CreateElement("Age");
+            ageNode.InnerText = "19";
+            employeeNode.AppendChild(lastNameNode);
+            employeeNode.AppendChild(ageNode);
+            root.AppendChild(employeeNode);
+
+            employeeNode = doc.CreateElement("Employee");
+            attribute = doc.CreateAttribute("Name");
+            attribute.Value = "Yadira";
+            employeeNode.Attributes.Append(attribute);
+            lastNameNode = doc.CreateElement("LastName");
+            lastNameNode.InnerText = "Quiroz";
+            ageNode = doc.CreateElement("Age");
+            ageNode.InnerText = "19";
+            employeeNode.AppendChild(lastNameNode);
+            employeeNode.AppendChild(ageNode);
+            root.AppendChild(employeeNode);
+
+            employeeNode = doc.CreateElement("Employee");
+            attribute = doc.CreateAttribute("Name");
+            attribute.Value = "Daylin";
+            employeeNode.Attributes.Append(attribute);
+            lastNameNode = doc.CreateElement("LastName");
+            lastNameNode.InnerText = "Cruz";
+            ageNode = doc.CreateElement("Age");
+            ageNode.InnerText = "20";
+            employeeNode.AppendChild(lastNameNode);
+            employeeNode.AppendChild(ageNode);
+            root.AppendChild(employeeNode);
+
+            doc.Save("example.xml");
+        }
+        private void btnRead_Click_1(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\keila\source\repos\AppXML\AppXML\bin\Debug\example.xml";
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml(filePath);
+            dgvData.DataSource = dataSet.Tables[0];
+            WriteXmlDocument();
+        }
+    }
+
+}
